979eb7213a5aeb3350aad6c41a6e7e14a68cb4a4
abfaye <mathsamidadi@gmail.com>
2019-11-29 02:42:05 +0100

    modifi

--- Java Projet/src/com/saliou/JavaProjet/MainApp.java
+++ Java Projet/src/com/saliou/JavaProjet/MainApp.java
@@ -8,7 +8,14 @@ public class MainApp extends Application{
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
+		primaryStage.setTitle("java Projet");
+		primaryStage.setWidth(1000);
+		primaryStage.setHeight(700);
 		primaryStage.show();
-	}
 
+		
+	}
+public static void main(String[] args) {
+	launch(args);
+}
 }
